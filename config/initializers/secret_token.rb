9a961fe2913adebb69f6636e28314658be6193e2
jweltseaga <jwelt@seagamfg.com>
2011-09-19 13:32:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Websites::Application.config.secret_token = 'fc1f1358b11e44c6ef2d815f04e48181b514b2fd2125597bf59d603cbc6e2ae920178bc952e65402f86a1e7c286b722eda21cdb7104ccb2b00454e651696c08b'
